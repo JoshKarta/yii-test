9e1c20db3ec5160a8725987c86a479d09e6e8555
DevKabinet <dev.kabinet@gmail.com>
2025-09-29 08:49:50 -0300

    test page for backend

---
+++ backend/views/test/_form.php
@@ -0,0 +1,25 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Test */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="test-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'value')->textInput(['maxlength' => true]) ?>
+
+  
+	<?php if (!Yii::$app->request->isAjax){ ?>
+	  	<div class="form-group">
+	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+	    </div>
+	<?php } ?>
+
+    <?php ActiveForm::end(); ?>
+    
+</div>
---
+++ backend/views/test/update.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Test */
+?>
+<div class="test-update">
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
